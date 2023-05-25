bba048ade9e409a642610a57d2580fafd9e183f0
Elliott Johnson <elliotthkj1991@gmail.com>
2023-05-24 21:35:14 -0700

    Instacart replica assignment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import './App.css';
+import "./App.css";
+import splashImage from "./embedded/splash.webp";
+import TopBar from "./components/TopBar";
+import styled from "styled-components";
+import StoreListing from "./components/StoreListing";
+
+const Splash = styled.div`
+  width: 100vw;
+  height: 250px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(${splashImage});
+  background-color: #dfeed6;
+  position: absolute;
+  left: 0;
+  top: 40px;
+  right: 0;
+  z-index: -1;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <TopBar />
+      <Splash />
+      <StoreListing />
+    </>
   );
 }
 
